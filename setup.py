c2b7baa2260638595450568b73f74c7ea79f3b79
vangheem <vangheem@gmail.com>
2017-07-03 10:54:13 -0500

    Back to development: 1.0.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ desc = 'A Python logging handler for Fluentd event collector'
 
 setup(
   name='aiofluent',
-  version='1.0.1',
+  version='1.0.2.dev0',
   description=desc,
   long_description=open(README).read(),
   package_dir={'aiofluent': 'aiofluent'},
